3cbd1332442658d842d09425ea61a96e11b2a02a
Felipe <sfortizrodriguez@gmail.com>
2026-07-21 13:02:10 -0400

    commit inicial

---
+++ Ejercicios Insystech C#/Controllers/Ejercicio1Controller.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Ejercicios_Insystech_C_.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class Ejercicio1Controller : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult ProbarConectividad()
+        {
+            string mensaje = "La conexión ha sido establecida con éxito";
+            return Ok(new { msg = mensaje });
+        }
+    }
+}
---
+++ Ejercicios Insystech C#/Program.cs
@@ -0,0 +1,37 @@
+using Microsoft.Extensions.Options;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddOpenApi();
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("PoliticaCORS", app =>
+    {
+        app.AllowAnyOrigin()
+        .AllowAnyHeader()
+        .AllowAnyMethod();
+    });
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+}
+
+app.UseHttpsRedirection();
+
+app.UseCors("PoliticaCORS");
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
