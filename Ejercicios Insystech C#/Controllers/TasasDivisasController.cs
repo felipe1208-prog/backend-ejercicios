4b735fd29aefb9f5fa953e0594639e6da207c28e
Felipe <sfortizrodriguez@gmail.com>
2026-07-29 10:16:06 -0400

    get para obtener registros de tasas

--- Ejercicios Insystech C#/Controllers/TasasDivisasController.cs
+++ Ejercicios Insystech C#/Controllers/TasasDivisasController.cs
@@ -1,7 +1,10 @@
 ﻿using Ejercicios_Insystech_C_.DTOs;
 using Ejercicios_Insystech_C_.Services;
+using Microsoft.AspNetCore.Components.Forms;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Linq;
 
 namespace Ejercicios_Insystech_C_.Controllers
 {
@@ -11,11 +14,12 @@ namespace Ejercicios_Insystech_C_.Controllers
     {
         private static AlmacenTasas _almacen;
 
-        public TasasDivisasController( AlmacenTasas almacen)
+        public TasasDivisasController(AlmacenTasas almacen)
         {
             _almacen = almacen;
         }
 
+        //Endpoint de registro de tasa en vista inicio
         [HttpPost]
         public async Task<IActionResult> GuardarTasa([FromBody] TasaDivisaDTO request)
         {
@@ -27,6 +31,35 @@ namespace Ejercicios_Insystech_C_.Controllers
             _almacen.TasasRegistradas.Add(request);
 
             return Ok(new { mensaje = "Tasa Registrada" });
+        }
+
+
+        //Endpoint para las 3 vistas de las divisas
+        [HttpGet("{tasa}")]
+        public async Task<IActionResult> ObtenerTasasRegistradas(string tasa)
+        {
+            if (tasa == null)
+            {
+                return BadRequest();
+            }
+
+            var tasasRegistradaPorDivisa = _almacen.TasasRegistradas
+                .Where(x => x.Tasa == tasa)
+                .Select(x => new TasaDivisaDTO
+                {
+                    FechaActualizacion = x.FechaActualizacion,
+                    Tasa = x.Tasa,
+                    Precio = x.Precio,
+                    Moneda = x.Moneda
+                })
+                .ToList();
+
+            if (!tasasRegistradaPorDivisa.Any()) 
+            {
+                return NotFound(new { mensaje = "Aún no hay registros para esta tasa. Te invitamos a registrar una en la sección 'Tasas del Día'" });
+            }
+
+            return Ok(tasasRegistradaPorDivisa);
         }
     }
 }
