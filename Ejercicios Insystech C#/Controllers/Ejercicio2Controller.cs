35249bcbb79197528e05a23bd1e3c66a5bc9e1db
Felipe <sfortizrodriguez@gmail.com>
2026-08-03 11:00:39 -0400

    backend completo ej 3

---
+++ Ejercicios Insystech C#/Controllers/Ejercicio2Controller.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Ejercicios_Insystech_C_.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class Ejercicio2Controller : ControllerBase
+    {
+        [HttpPost("{sueldo}")]
+        public async Task<IActionResult> CalculoSueldo([FromQuery] decimal? sueldo)
+        {
+            if (sueldo == null || sueldo <= 0)
+            {
+                return BadRequest(new { MensajeError400 = "Introduzca un sueldo válido" });
+            }
+
+            var sueldoLuegoImpuestos = sueldo - (sueldo * 0.055m);
+
+            return Ok(sueldoLuegoImpuestos);
+        }
+    }
+}
