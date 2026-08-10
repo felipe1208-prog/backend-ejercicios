2d0b6f21c67861fffc091502bfcde0d2601e7c50
Felipe <sfortizrodriguez@gmail.com>
2026-08-10 09:58:06 -0400

    dto usuario

---
+++ Ejercicios Insystech C#/DTOs/UsuarioDTO.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Ejercicios_Insystech_C_.DTOs
+{
+    public class UsuarioDTO
+    {
+        [Required(ErrorMessage = "El campo 'Nombre' es requerido.")]
+        public string Nombre { get; set; }
+
+        [Required(ErrorMessage = "El campo 'Correo Electrónico' es requerido.")]
+        [EmailAddress(ErrorMessage = "Ingrese un correo válido.")]
+        public string CorreoElectrónico { get; set; }
+
+        [Required(ErrorMessage = "La Contraseña es necesaria.")]
+        public string Clave { get; set; }
+    }
+}
