73c4e3a7f532c277fd82b33154522addf6b05e8e
Felipe <sfortizrodriguez@gmail.com>
2026-08-10 11:20:59 -0400

    metodos servicio 5

--- Ejercicios Insystech C#/DTOs/UsuarioDTO.cs
+++ Ejercicios Insystech C#/DTOs/UsuarioDTO.cs
@@ -2,16 +2,17 @@
 
 namespace Ejercicios_Insystech_C_.DTOs
 {
+    //Las propiedades del DTO se hacen nulables para que el error 400 no sea el generico de .NET sino el que yo coloqué
     public class UsuarioDTO
     {
         [Required(ErrorMessage = "El campo 'Nombre' es requerido.")]
-        public string Nombre { get; set; }
+        public string? Nombre { get; set; }
 
         [Required(ErrorMessage = "El campo 'Correo Electrónico' es requerido.")]
         [EmailAddress(ErrorMessage = "Ingrese un correo válido.")]
-        public string CorreoElectrónico { get; set; }
+        public string? CorreoElectronico { get; set; }
 
         [Required(ErrorMessage = "La Contraseña es necesaria.")]
-        public string Clave { get; set; }
+        public string? Clave { get; set; }
     }
 }
---
+++ Ejercicios Insystech C#/Services/LoginService.cs
@@ -0,0 +1,39 @@
+﻿using Ejercicios_Insystech_C_.DTOs;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+
+namespace Ejercicios_Insystech_C_.Services
+{
+    public class LoginService
+    {
+        private readonly List<UsuarioDTO> UsuarioRegistrados = new List<UsuarioDTO>
+        {
+            new UsuarioDTO { Nombre = "Felipe", CorreoElectronico = "dev@insystech.com.ve", Clave = "Inicial2026*." },
+            new UsuarioDTO { Nombre = "Pascia", CorreoElectronico = "dev@insystech.com.ve", Clave = "Inicial2026*." },
+            new UsuarioDTO { Nombre = "Freddy", CorreoElectronico = "dev@insystech.com.ve", Clave = "Inicial2026*." },
+            new UsuarioDTO { Nombre = "Alejandro", CorreoElectronico = "dev@insystech.com.ve", Clave = "Inicial2026*." },
+            new UsuarioDTO { Nombre = "Jose", CorreoElectronico = "dev@insystech.com.ve", Clave = "Inicial2026*." },
+            new UsuarioDTO { Nombre = "Diego", CorreoElectronico = "dev@insystech.com.ve", Clave = "Inicial2026*." },
+            new UsuarioDTO { Nombre = "Gilman", CorreoElectronico = "gperez@insystech.com.ve", Clave = "Gilmanito007" },
+            new UsuarioDTO { Nombre = "Victor", CorreoElectronico = "vpiedra@insystech.com.ve", Clave = "Lucky" },
+            new UsuarioDTO { Nombre = "Gregory", CorreoElectronico = "ggomez@insystech.com.ve", Clave = "Strike" },
+            new UsuarioDTO { Nombre = "David", CorreoElectronico = "dperez@insystech.com.ve", Clave = "Dart123" },
+            new UsuarioDTO { Nombre = "Jesus", CorreoElectronico = "jparedes@insystech.com.ve", Clave = "Inicial2024*" },
+            new UsuarioDTO { Nombre = "Javier", CorreoElectronico = "jrojas@insystech.com.ve", Clave = "RojasTrading" },
+            new UsuarioDTO { Nombre = "Francisco", CorreoElectronico = "fchacon@insystech.com.ve", Clave = "Coordinator" },
+            new UsuarioDTO { Nombre = "Edgar", CorreoElectronico = "eparedes@insystech.com.ve", Clave = "1" }
+        };
+
+        public string VerificacionUsuario(UsuarioDTO request)
+        {
+            var usuarioEncontrado = UsuarioRegistrados.FirstOrDefault(usuario => usuario.CorreoElectronico == request.CorreoElectronico);
+
+            if (usuarioEncontrado == null) return "Usuario no Encontrado";
+            if (usuarioEncontrado.Nombre != request.Nombre) return $"El nombre '{request.Nombre}' es inválido.";
+            if (usuarioEncontrado.Nombre != request.Nombre) return $"La contraseña es incorrecta.";
+
+            return $"Bienvenido {request.Nombre}!. Inciaste sesión correctamente.";
+        }
+    }
+
+}
